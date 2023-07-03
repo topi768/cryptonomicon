0ad204a8f73115c982838ee9e8972ca19bab7cc9
topi768 <90519641+topi768@users.noreply.github.com>
2023-07-03 15:27:36 +1000

    start 11 urok

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,144 @@
 <template>
- <div></div>
+<div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+
+  <div class="container">
+    <section>
+      <div class="flex">
+        <div class="max-w-xs">
+          <label for="wallet" class="block text-sm font-medium text-gray-700"
+            >Тикер</label
+          >
+          <div class="mt-1 relative rounded-md shadow-md">
+            <input
+              v-model="ticker"
+              v-on:keydown.enter='add'
+              type="text"
+              name="wallet"
+              id="wallet"
+              class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder="Например DOGE"
+            />
+          </div>
+          
+          <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BTC
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              DOGE
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              BCH
+            </span>
+            <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+              CHD
+            </span>
+          </div>
+          <div class="text-sm text-red-600">Такой тикер уже добавлен</div>
+        </div>
+      </div>
+      <button
+        @click="add()"
+        type="button"
+        class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+      >
+        <!-- Heroicon name: solid/mail -->
+        <svg
+          class="-ml-0.5 mr-2 h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="#ffffff"
+        >
+          <path
+            d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
+          ></path>
+        </svg>
+        Добавить
+      </button>
+    </section>
+    <template
+      v-if="tickers.length > 0"
+      >
+        <hr class="w-full border-t border-gray-600 my-4" />
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div
+            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+            v-for="t in tickers"
+            :key="t.name"
+          >
+            <div class="px-4 py-5 sm:p-6 text-center">
+              <dt class="text-sm font-medium text-gray-500 truncate">
+                {{t.name}}  - USD
+              </dt>
+              <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                {{t.price}}
+              </dd>
+            </div>
+            <div class="w-full border-t border-gray-200"></div>
+            <button
+              @click="remove(t)"
+              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+            >
+              <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#718096"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                  clip-rule="evenodd"
+                ></path></svg>Удалить
+            </button>
+          </div>
+          
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4" />
+    </template>
+  </div>
+</div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      ticker: 'default',
+      tickers: [
+        {name:'WTF', price:'-' },
+        {name:'VUE', price:'-' },
+        {name:'BTC', price:'-' },
+        // {title:'DOGE - USD', price:'-' },
+        
+      ]
+
+    }
+  },
+  methods: {
+    add(){
+      const newTicker = {
+        name: this.ticker,
+        price:'-'
+      }
+
+      this.tickers.push(newTicker)
+      this.ticker = ''
+    },
+    remove(tickerToRemove){
+      this.tickers = this.tickers.filter(t => t !=tickerToRemove)
+    }
+
+
+  }
+}
+</script>
+
+<style src='./app.css'>
 
 </style>
